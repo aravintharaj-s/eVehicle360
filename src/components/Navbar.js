d58ae93819c65132230ad4063740bad1f26d8c09
Aravintharaj S <aravioct@gmail.com>
2022-04-26 21:52:29 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,31 @@
-import { MdElectricBike } from "react-icons/md";
+import { MdElectricBike,MdOutlineShoppingCart,MdFavorite,MdSearch } from "react-icons/md";
 const Navbar = () => {
     return ( 
       <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">
+      <MdElectricBike color="green"/>
+      eVehiclez360
+      </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse justify-content-end align-center" id="navbarNavAltMarkup">
+    <div className="collapse navbar-collapse justify-content-center align-center" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="/">Features</a>
-        <a className="nav-link" href="/">Pricing</a>
-        <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a className="nav-link "  href="#">E-Scooters</a>
+        <a className="nav-link" href="/">E-Bikes</a>
+        <a className="nav-link" href="/">Franchisee</a>
+        <a className="nav-link "  href="#">Careers</a>
+        <a className="nav-link" href="/">Inside eVehiclez360</a>
+        </div>
+        <div className="collapse navbar-collapse justify-content-end align-center" id="navbarNavAltMarkup">
+        <button type="button" class="btn btn-sm btn-success">Login/Register</button>
+        <a className="nav-link "  href="#"><MdSearch color="black" size={'30px'}/></a>
+        <a className="nav-link "  href="#"><MdFavorite color="crimson" size={'30px'}/></a>
+        <a className="nav-link" href="/"><MdOutlineShoppingCart color="green" size={'30px'}/></a>
+        
+        
       </div>
     </div>
   </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import Navbar from "../components/Navbar";
 import Contact from "../components/Contact";
+import './Home.css'
 const Home = () => {
     return ( 
         <div>
             <Navbar/>
-            Main content
+            <div className="gg ">Main content</div>
             <Contact/>
         </div>
      );
